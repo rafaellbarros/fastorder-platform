297e2a675f6b284ee8d3d149841ac77c0afbe19f
Rafael Barros <rafaelbarros.df@gmail.com>
2026-01-27 00:27:22 -0300

    📦 refactor: remove user feign client and related gateway logic

--- gateway/src/main/java/br/com/rafaellbarros/fastorder/api/gateway/dto/request/CreateUserRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package br.com.rafaellbarros.fastorder.api.gateway.dto.request;
-
-import lombok.Data;
-
-@Data
-public class CreateUserRequestDTO {
-    private String name;
-    private String email;
-}
